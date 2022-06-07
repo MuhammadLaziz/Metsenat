b488ec3e164b14c482c2ecea871574425239546a
MuhammadLaziz <ganievjsdeveloper@gmail.com>
2022-06-07 12:32:35 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header >
+   <p class="text-red-500">sasasas</p>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+</style>
